0646984ebb21f8895bb0160824aeac931551c79b
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-08-15 23:06:37 +0200

    add sections in landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -4,7 +4,9 @@ import About from '../_sections/about';
 import Description from '../_sections/description';
 import Hero from '../_sections/hero';
 import { Footer } from '../_sections/footer';
-
+import Portfolio from '../_sections/portfolio';
+import Contact from '../_sections/contact';
+import PriceSection from '../_sections/price';
 
 const LandingPage = () => {
 	return (
@@ -12,6 +14,9 @@ const LandingPage = () => {
 			<Hero />
 			<About />
 			<Description />
+			<Portfolio />
+			<Contact />
+			<PriceSection />
 			<Footer />
 		</div>
 	);
