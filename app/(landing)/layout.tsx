bd6d00f2344402e8c5f67a59e99f63f24712efb3
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-08-15 14:01:26 +0200

    add layout

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BackgroundImage } from './_components/background-image';
+import { Navbar } from './_components/navbar';
+interface LandingLayoutProps {
+	children: React.ReactNode;
+}
+const LandingLayout = ({ children }: LandingLayoutProps) => {
+	return (
+		<>
+			<BackgroundImage />
+			<Navbar />
+			{children}
+		</>
+	);
+};
+
+export default LandingLayout;
