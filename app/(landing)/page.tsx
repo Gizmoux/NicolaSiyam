b9a2e4b9a786419d8987367bdf71c75e5d922cc4
Gizmoux <nicolas.deriemacker@hotmail.fr>
2024-08-16 11:43:15 +0200

    update landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-
 import About from '../_sections/about';
 import Description from '../_sections/description';
 import Hero from '../_sections/hero';
 import { Footer } from '../_sections/footer';
 import Portfolio from '../_sections/portfolio';
-import Contact from '../_sections/contact';
 import PriceSection from '../_sections/price';
+import Contact from '../_sections/contact';
 
 const LandingPage = () => {
 	return (
 		<div className="">
-			<Hero />
-			<About />
-			<Description />
+			<Hero  />
+			<About/>
+			<Description/>
 			<Portfolio />
-			<Contact />
+			<Contact/>
 			<PriceSection />
 			<Footer />
 		</div>
